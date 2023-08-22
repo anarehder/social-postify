db1de90a79f2cb8d857347ce11b889c3b1d1b674
anarehder <anapaula.rehder@gmail.com>
2023-08-22 09:24:50 -0300

    feat/ publications dtos

--- src/publications/dto/create-publication.dto.ts
+++ src/publications/dto/create-publication.dto.ts
@@ -1,1 +1,23 @@
-export class CreatePublicationDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+
+export class CreatePublicationDto {
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty({
+    message: 'All fields are required!',
+  })
+  mediaId: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty({
+    message: 'All fields are required!',
+  })
+  postId: number;
+
+  @IsDate()
+  @IsNotEmpty({
+    message: 'All fields are required!',
+  })
+  date: Date;
+}
