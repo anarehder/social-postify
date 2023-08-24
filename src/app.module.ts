a13031f18e6baf5795b035733597a5da2bc5165f
anarehder <anapaula.rehder@gmail.com>
2023-08-24 07:57:59 -0300

    feat/ prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { MediasModule } from './medias/medias.module';
 import { PostsModule } from './posts/posts.module';
 import { PublicationsModule } from './publications/publications.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [MediasModule, PostsModule, PublicationsModule],
+  imports: [MediasModule, PostsModule, PublicationsModule, PrismaModule],
 })
 export class AppModule {}
